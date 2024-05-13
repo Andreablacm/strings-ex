3953f2ae287eb0565bbae102be5d71e8211cb181
Victor <163881094+Andreablacm@users.noreply.github.com>
2024-05-13 18:52:51 +0200

    :)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+function upperThan5(word) {
+  if (word.lengh > 5) {
+    console.log(word.toLowerCase());
+  } else console.log(word.toUpperCase());
+}
+
+upperThan5('Amore');
+
+function upperThan5Ternary(word) {
+  word.lengh >= 5 ? console.log(word.toLowerCase()) : console.log(word.toUpperCase());
+}
+
+upperThan5Ternary('Amor');
+
+//number >= 4 ? console.log('el número es mayor o igual a 4') : console.log('El número es menor de 4');
